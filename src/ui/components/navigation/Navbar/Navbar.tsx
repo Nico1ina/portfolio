d60ba1fd7e99c1ed9e6ceee0a48cb0f6ce72207e
Nicolina Larsson <oskarjennefors@c-aa48e655.04-305-73746f71.bbcust.telenor.se>
2024-02-29 15:40:18 +0100

    updates to portfolio

--- src/ui/components/navigation/Navbar/Navbar.tsx
+++
@@ -1,9 +0,0 @@
-export default function Navbar() {
-  return (
-    <nav className="">
-      <div className="text-black">
-        <p>©/2024</p>
-      </div>
-    </nav>
-  );
-}
--- src/ui/components/utility/index.ts
+++
@@ -1,3 +0,0 @@
-/* Utility components serve to provide functionality to a project (although are unlikely to feature in a design system) */
-
-export {};
