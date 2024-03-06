9bd5fdcac8eaf2ea3ea4fd3c77420a7bd01ef0c5
Fabrice Werger <fabricewerger@icloud.com>
2024-03-06 15:59:55 +0100

    concept text animation

--- app/page.tsx
+++ app/page.tsx
@@ -3,8 +3,20 @@ import Footer from "@/src/ui/components/navigation/Footer/Footer";
 
 export default function Home() {
   return (
-    <div className="bg-[#ece7e1] flex flex-col min-h-screen justify-between">
+    <div className="bg-[#ece7e1] flex flex-col min-h-screen justify-between overflow-hidden relative">
       <Menu />
+      <div className="overflow-hidden h-10 relative group">
+        <div className="animate-slide-down absolute bottom-10 left-0 w-full h-full flex items-center justify-center">
+          <h1 className="text-4xl font-bold text-white group-hover:animate-fall">
+            Sliding Text
+          </h1>
+        </div>
+        <div className="animate-slide-up absolute bottom-0 left-0 w-full h-full flex items-center justify-center">
+          <h1 className="text-4xl font-bold text-white group-hover:animate-fall">
+            Sliding Text
+          </h1>
+        </div>
+      </div>
       <Footer />
     </div>
   );
